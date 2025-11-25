71ae96ec9f83cca359ee2624aae7d6ee40b4e0a3
kiel-hub <kielnanggala@gmail.com>
2025-11-26 00:00:32 +0700

    Add files via upload

---
+++ Soal3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    char a[20]="Teknik";
+    char b[20]="Informatika";
+    strcat(a,b);
+
+    cout << a << endl;
+}
---
+++ strlen.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char huruf[20];
+    char pindah[20];
+    //clrscr();
+
+    cout << "Masukkan Sembarang Kata = ";
+    cin >> huruf;
+
+    cout << "Panjang Kata Yang Diinputkan = ";
+    cout << strlen(huruf) << endl;
+
+    getchar();
+}
